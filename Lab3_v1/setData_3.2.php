711319f41e67f22cc678f0a566ebc22b039cc0bf
elzalatypova <79845168+elzalatypova@users.noreply.github.com>
2022-05-19 00:01:53 +0500

    Add files via upload

---
+++ Lab3_v1/setData_3.2.php
@@ -0,0 +1,12 @@
+﻿<?php
+    $mysqli = new mysqli("localhost", "root", "", "comments");
+    if ($mysqli->connect_errno) {
+echo "Не удалось подключиться к БД";
+}
+    $email = $_GET['email'];
+    $name = $_GET['name'];
+    $message = $_GET['message'];
+    $result = $mysqli->query(
+        "INSERT INTO comm SET name='$name', email='$email', message='$message'"
+    );
+?>
